ddd85730ee6fab0f512ddebe5cf60486b20e124e
hasbrour1-wk <roberthasbrouck@gmail.com>
2016-01-05 11:59:38 -0500

    Finished

---
+++ TestApp1/Form1.Designer.cs
@@ -0,0 +1,95 @@
+﻿namespace TestApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.folderpath = new System.Windows.Forms.Button();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.label2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(40, 149);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(0, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // folderpath
+            // 
+            this.folderpath.Location = new System.Drawing.Point(99, 82);
+            this.folderpath.Name = "folderpath";
+            this.folderpath.Size = new System.Drawing.Size(127, 31);
+            this.folderpath.TabIndex = 1;
+            this.folderpath.Text = "Browse for file...";
+            this.folderpath.UseVisualStyleBackColor = true;
+            this.folderpath.Click += new System.EventHandler(this.folderpath_Click);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            this.openFileDialog1.FileOk += new System.ComponentModel.CancelEventHandler(this.openFileDialog1_FileOk);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(48, 18);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(242, 50);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Alter Rockland EDD \r\nto match wanted Format";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(332, 237);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.folderpath);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Rockland Formatter";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button folderpath;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ TestApp1/Form1.cs
@@ -0,0 +1,169 @@
+﻿using System;
+using System.IO;
+using System.Diagnostics;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Excel = Microsoft.Office.Interop.Excel;
+
+/*
+ * @author: Robert Hasbrouck
+ * @version: 1.3
+ */
+
+namespace TestApp1
+{
+    public partial class Form1 : Form
+    {
+
+        public static Excel._Workbook tmpBook = null;
+        public static Excel.Application tmpApp = null;
+        public static Excel._Worksheet tmpSheet = null;
+        public static Excel._Workbook srcBook = null;
+        public static Excel.Application srcApp = null;
+        public static Excel._Worksheet srcSheet = null;
+
+        private string filename = "";   // Output file name	
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void folderpath_Click(object sender, EventArgs e)
+        {
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                String filepath = openFileDialog1.FileName;
+                this.label1.Text = "Opening " + filepath;
+                Execute(filepath);
+                this.label1.Text = "Complete";
+
+            }
+        }
+        
+        private void openFileDialog1_FileOk(object sender, CancelEventArgs e)
+        {
+           
+        }
+
+        //Execute Method takes the file selected and formats it 
+        //to the rockland template specification.  The new excel 
+        //workbook is then saved in the same directory as the file
+        //that was selected.
+        public void Execute(String path)
+        {
+
+            filename = path;
+
+            //templatePath will be where template.xls will be stored
+            //source path get from browse button
+            //appPath is directory the .exe is located, which the template should be in same directory
+            string appPath = Application.StartupPath;
+            string templatePath, sourcePath;
+
+            //root of exe plus template name
+            appPath += "\\template.xls";
+            
+            templatePath = @appPath;
+            sourcePath = @filename;
+
+            //If the template file is found, run program
+            if (File.Exists(templatePath) && File.Exists(sourcePath))
+            {
+                //Template setup
+                tmpApp = new Excel.Application();
+                tmpApp.Visible = false;
+                tmpBook = (Excel._Workbook)(tmpApp.Workbooks.Add(templatePath));
+                tmpSheet = (Excel._Worksheet)tmpBook.Sheets[1];
+
+                //source setup
+                srcApp = new Excel.Application();
+                srcApp.Visible = false;
+                srcBook = (Excel._Workbook)(srcApp.Workbooks.Add(sourcePath));
+                srcSheet = (Excel._Worksheet)srcBook.Sheets[1];
+
+                //Merge Both files here
+                for (int i = 1; i < 33; i++)
+                {
+                    string str = srcSheet.Cells[2, i].Value2;
+                    tmpSheet.Cells[3, i] = str;
+
+                }
+
+                //Results page
+                tmpSheet = (Excel._Worksheet)tmpBook.Sheets[2];
+                srcSheet = (Excel._Worksheet)srcBook.Sheets[2];
+
+                int mod = 1;
+
+                for (int i = 2; i <= srcSheet.UsedRange.Rows.Count; i++)
+                {
+                    //skip FLDTMP Row
+                    if (srcSheet.Cells[i, 1].Value2 != "FLDTEMP")
+                    {
+                        for (int j = 1; j < 16; j++)
+                        {
+                            string str = srcSheet.Cells[i, j].Value2;
+                            tmpSheet.Cells[i + mod, j] = str;
+                        }
+                        //re size row height 
+                        tmpSheet.Cells[i + 1, 1].RowHeight = 12.75;
+                    }
+                    else
+                    {
+                        mod = 0;
+                    }
+                }
+
+                object m_objOpt = System.Reflection.Missing.Value;
+
+                //remove extension of filename
+                string[] remove = { ".xls", ".xlsx"};
+
+                foreach (string item in remove)
+                    if (filename.EndsWith(item))
+                    {
+                        filename = filename.Substring(0, filename.LastIndexOf(item));
+                        break; //only allow one match at most
+                    }
+
+                //save workbook
+                tmpBook.SaveAs(@filename + "_Formatted.xls", m_objOpt,
+                        m_objOpt, m_objOpt, true, m_objOpt, Excel.XlSaveAsAccessMode.xlNoChange,
+                        m_objOpt, m_objOpt, m_objOpt, m_objOpt, m_objOpt);
+                tmpBook.Close(true);
+                tmpApp.Workbooks.Close();
+                tmpApp.Quit();
+                srcBook.Close(true);
+                srcApp.Workbooks.Close();
+                srcApp.Quit();  
+                //Open file location after saved
+                if (File.Exists(@filename + "_Formatted.xls"))
+                {
+                    Process.Start("explorer.exe", "/select, " + @filename + "_Formatted.xls");
+                }
+
+            }
+            else
+            {
+                this.label1.Text = "Error: File not found.  Please Try again.";
+            }
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
